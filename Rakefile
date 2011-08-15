508a059b88ee00ec78ffc8d3a5e5dd3279bbe168
Patrick Huesler <patrick.huesler@gmail.com>
2011-08-15 16:39:23 +0200

    super basic prototype with a udp socket that pushes to a websocket

--- Rakefile
+++ Rakefile
@@ -1,2 +1,14 @@
 require 'bundler'
 Bundler::GemHelper.install_tasks
+
+desc 'send test data'
+task :send_test_data do
+  require 'socket'
+  require 'JSON'
+  socket = UDPSocket.new
+  test_data = { :type => 'counter', :name => 'usersOnline', :value => 1 }
+  while(true)
+   socket.send(JSON.generate(test_data), 0, '127.0.0.1', 3000)
+   sleep(3)
+  end
+end
--- lib/yada.rb
+++ lib/yada.rb
@@ -2,15 +2,33 @@ require 'em-websocket'
 
 module Yada
   class Server
+    class UdpConnection < EventMachine::Connection
+      
+      MESSAGE_DELIMITER = "\r\n"
+
+      def initialize(*args)
+        @server = args[0]
+        super *args
+      end
+
+      def receive_data(data)
+        if data && data.size > 0
+          @server.channel.push("#{data}#{MESSAGE_DELIMITER}")
+        end
+      end
+    end
+
     def self.run
+      new.run
+    end
+
+    def run
       EventMachine.run {
         @channel = EM::Channel.new
 
-        EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080, :debug => true) do |ws|
+        EventMachine::open_datagram_socket("127.0.0.1", 3000, UdpConnection, self)
 
-         timer = EventMachine::PeriodicTimer.new(5) do
-          @channel.push 'yada yada yada'
-         end
+        EventMachine::WebSocket.start(:host => "0.0.0.0", :port => 8080, :debug => true) do |ws|
 
           ws.onopen {
             sid = @channel.subscribe{|msg| ws.send msg}
@@ -23,8 +41,12 @@ module Yada
         end
 
         puts "Server started"
-      }
-   end
+      }      
+    end
+
+    def channel
+      @channel
+    end
   end
 end
 
